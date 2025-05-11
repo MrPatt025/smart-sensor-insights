1568ce2c9e1f23638bc793d961497bab8bec408e
MrPatt025 <prawarna10@gmail.com>
2025-05-11 10:21:19 +0700

    Enforce LF line endings via .gitattributes

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"], // Updated content paths
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
